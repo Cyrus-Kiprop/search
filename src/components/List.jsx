0a20ba643c6c9c07596312a28532b70060fd1eee
Cyrus-Kiprop <cyruskiprop254@gmail.com>
2019-10-28 16:43:00 +0300

    add search button

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const List = ({ tasks, onDelete }) => {
+  const [filter, setFilter] = useState(tasks);
+
+  //handle filter
+  const handleSearch = e => {
+    e.preventDefault();
+    // some code
+    let currentList = [];
+    let newList = [];
+
+    if (e.target.value !== "") {
+      currentList = tasks;
+      newList = currentList.filter(item =>
+        item.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+    } else if (e.target.value === "") {
+      setFilter(tasks);
+    }
+
+    console.log(newList);
+    //   update the filter state
+    setFilter([...newList]);
+  };
+
   return (
-    <ul>
-      {tasks.map((a, i, arr) => (
-        <li key={i}>
-          {" "}
-          {a}
-          &nbsp;
-          <span className="delete" onClick={() => onDelete(a)} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <input
+        type="text"
+        className="imput"
+        placeholder="search..."
+        onChange={handleSearch}
+      />
+      <ul>
+        {filter.map((a, i, arr) => (
+          <li key={i}>
+            {" "}
+            {a}
+            &nbsp;
+            <span className="delete" onClick={() => onDelete(a)} />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
